793501f25bf0e558142037ea60838400d585029e
DJMarshallSantos <marceloaquinodossantos@gmail.com>
2022-03-27 13:23:02 -0400

    heroku final

--- README.md
+++ README.md
@@ -2,6 +2,6 @@
 
 Este projeto foi feito em conjunto: Autores Hilton Jun Honda(Git: HiltonJun) e Marcelo A. Santos(Git: DJMarshallSantos).
 
-O projeto consiste em uma página da web feito no Visual Studio Code, com tecnologias CSS e Express e foi hospedado no servidor Heroku (https://pokedex-bluedtech.herokuapp.com/)
+O projeto consiste em uma página da web feito no Visual Studio Code, com tecnologias CSS e Express e foi hospedado no servidor Heroku (https://blueedtchpokedex.herokuapp.com/)
 
 A página é uma Pokédex beta na qual o usuário pode cadastrar pokémons e visualizar seus dados como nome, número, tipo, entre outros e fazer edições nos seus cadastros. 
--- public/js/script.js
+++ public/js/script.js
@@ -14,45 +14,12 @@ function currentSlide(n) {
 function showSlides(n) {
   let i;
   let slides = document.getElementsByClassName("pokemon-card");
-  console.log(slides.length)
-  if (n > slides.length) { slideIndex = 1 }
-  if (n < 1) { slideIndex = slides.length }
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
   for (i = 0; i < slides.length; i++) {
     slides[i].style.display = "none";
   }
-  if (slides.length > 0) {
-    slides[slideIndex - 1].style.display = "block";
-  }
-}
-
-function displayImg() {
-  let show = document.getElementById("display-img");
-  show.innerHTML = "";
-  const imgUrl = document.getElementById("url").value;
-  let img = document.createElement("img");
-  img.src = imgUrl;
-  show.appendChild(img);
-}
-
-const closeMessage = document.querySelector("#close");
-const message = document.querySelector("#message");
-
-if (closeMessage) {
-  closeMessage.addEventListener("click", function () {
-    message.style.display = "none";
-  });
-}
-
-
-setTimeout(() => {
-  if (message) {
-    message.style.display = "none"
-  }
-}, 5000);
-
-function remove(el) {
-  const element = el;
-  element.remove();
+  slides[slideIndex-1].style.display = "block";
 }
 
 function displayImg() {
@@ -62,6 +29,16 @@ function displayImg() {
   img.src = imgUrl;
   show.appendChild(img);
 }
-console.log("loaded")
+
+const closeMessage = document.querySelector("#close");
+const message = document.querySelector("#message");
+
+closeMessage.addEventListener("click", function (){
+    message.style.display = "none"
+});
+
+setTimeout(() => {
+    message.style.display = "none"
+}, 5000); 
 
 
